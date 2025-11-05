c71aef645520094d4bd7e864e26929679303289f
LucaLeFich <pq48wkn@eduvaud.ch>
2025-11-05 12:15:42 +0100

    Challenges gadget step 1-3, 4 wip

---
+++ Pratique/vue-gadget-shop/main.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data() {
+        return{
+            title: 'Vue Gadget Shop',
+            description: 'Découvrez les derniers gadgets électroniques de haute technologie !',
+            gadgets: [
+                {id: 1, name: 'Smartphone XZ', price: '799$', image: 'assets/phone.jpg', inStock: true},
+                {id: 2, name: 'Laptop Pro', price: '1299$', image: 'assets/laptop.jpg', inStock: false},
+                {id: 3, name: 'Earbuds', price: '99.99$', image: 'assets/earbuds.jpg', inStock: true}
+            ]
+        }
+    }
+})
