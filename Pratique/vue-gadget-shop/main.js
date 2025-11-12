650292b666abfda6b2d46cd9550476873e7c236e
LucaLeFich <pq48wkn@eduvaud.ch>
2025-11-12 09:04:52 +0100

    Gadget Shop step 5

--- Pratique/vue-gadget-shop/main.js
+++ Pratique/vue-gadget-shop/main.js
@@ -7,7 +7,16 @@ const app = Vue.createApp({
                 {id: 1, name: 'Smartphone XZ', price: '799$', image: 'assets/phone.jpg', inStock: true},
                 {id: 2, name: 'Laptop Pro', price: '1299$', image: 'assets/laptop.jpg', inStock: false},
                 {id: 3, name: 'Earbuds', price: '99.99$', image: 'assets/earbuds.jpg', inStock: true}
-            ]
+            ],
+            cart: []
+        }
+    },
+    methods: {
+        addToCart(gadget) {
+            this.cart.push(gadget);
+        },
+        removeFromCart(index) {
+            this.cart.splice(index, 1);
         }
     }
 })
