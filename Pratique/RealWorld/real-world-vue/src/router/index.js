6bd1d6ac695bc7f0527a44eb20328edac02b408b
LucaLeFich <195764455+LucaLeFich@users.noreply.github.com>
2025-11-25 15:43:53 +0100

    Exercice REAL-WORLD-VUE (fini)

--- Pratique/RealWorld/real-world-vue/src/router/index.js
+++ Pratique/RealWorld/real-world-vue/src/router/index.js
@@ -1,14 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/EventListView.vue'
 import AboutView from '../views/AboutView.vue'
+import EventDetailView from '../views/EventDetailsView.vue'
+import EventListView from '../views/EventListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'event-list',
+      component: EventListView,
+    },
+    {
+      path: '/event/:id',
+      name: 'event-details',
+      props: true,
+      component: EventDetailView,
     },
     {
       path: '/about',
